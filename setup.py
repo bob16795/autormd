a25467f0aaf382b8257903a49b24b77e564e4fbe
bob16795 <prestosilver.ptp@gmail.com>
2019-06-01 19:13:28 -0400

    whoops

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     keywords = "example documentation tutorial",
     packages=['autormd'],
     long_description=read('README'),
-    data_files=[(os.env['HOME'] + '/Documents/inf', ['inf/doc_header', 'inf/essay_header']),
-                (os.env['HOME'] + '/Documents/src', ['src/main.rmd'])],
+    data_files=[(os.environ['HOME'] + '/Documents/inf', ['inf/doc_header', 'inf/essay_header']),
+                (os.environ['HOME'] + '/Documents/src', ['src/main.rmd'])],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
