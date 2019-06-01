9d2b7116c18508a10f32edc624b6500950376f4f
bob16795 <prestosilver.ptp@gmail.com>
2019-06-01 19:22:13 -0400

    cant work like that

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-os.makedirs(os.environ['HOME'] + '/Documents/src')
-os.makedirs(os.environ['HOME'] + '/Documents/inf')
-os.makedirs(os.environ['HOME'] + '/Documents/cit')
-os.makedirs(os.environ['HOME'] + '/Documents/pdf')
-
 setup(
     name = "autormd",
     version = "4.1",
@@ -16,8 +11,6 @@ setup(
     keywords = "example documentation tutorial",
     packages=['autormd'],
     long_description=read('README'),
-    data_files=[(os.environ['HOME'] + '/Documents/inf', ['inf/doc_header', 'inf/essay_header']),
-                (os.environ['HOME'] + '/Documents/src', ['src/main.rmd'])],
     install_requires=[
         'click',
         'pathlib',
