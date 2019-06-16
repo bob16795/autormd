6cfe4fde6d9af8bc7465f7d820177a5797d5749f
bob16795 <prestosilver.ptp@gmail.com>
2019-06-16 13:42:08 -0700

    fixed setup for linux

--- setup.py
+++ setup.py
@@ -3,25 +3,46 @@ import os
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(
-    name = "autodocx",
-    version = "0.1.0",
-    keywords = "example documentation tutorial",
-    packages=['autodocx', 'autodocx.formaters'],
-    long_description=read('README.md'),
-    install_requires=[
-        'click',
-        'pathlib',
-        'python-docx',
-        'pypiwin32'
-    ],
-    entry_points={
-        'console_scripts': ['autodocx = autodocx.cli:start']
-    },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
-)
+if os.name == 'nt':
+    setup(
+        name = "autodocx",
+        version = "0.1.0",
+        keywords = "example documentation tutorial",
+        packages=['autodocx', 'autodocx.formaters'],
+        long_description=read('README.md'),
+        install_requires=[
+            'click',
+            'pathlib',
+            'python-docx',
+            'pypiwin32'
+        ],
+        entry_points={
+            'console_scripts': ['autodocx = autodocx.cli:start']
+        },
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: BSD License",
+        ],
+    )
+else:
+    setup(
+        name = "autodocx",
+        version = "0.1.0",
+        keywords = "example documentation tutorial",
+        packages=['autodocx', 'autodocx.formaters'],
+        long_description=read('README.md'),
+        install_requires=[
+            'click',
+            'pathlib',
+            'python-docx'
+        ],
+        entry_points={
+            'console_scripts': ['autodocx = autodocx.cli:start']
+        },
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: BSD License",
+        ],
+    )
